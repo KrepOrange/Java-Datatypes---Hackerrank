6adc68c273b96bd6a325f09df30965920d34268a
İhsan B. Arıtürk <139109087+KrepOrange@users.noreply.github.com>
2024-03-04 15:11:59 +0300

    Update_2 IntegerRange2.java

--- IntegerRange2.java
+++ IntegerRange2.java
@@ -5,23 +5,22 @@ public class IntegerRange2 {
     public static void main(String[] args) {
         //variable
         Scanner range = new Scanner(System.in);
-        double theNumber;
+        DecimalFormat decFormat = new DecimalFormat("0.#");
 
         byte byteMin = (byte) (-1 * Math.pow(2, 7)), byteMax = (byte) (Math.pow(2, 7) - 1);
         short shortMin = (short) (-1 * Math.pow(2, 15)), shortMax = (short) (Math.pow(2, 15) - 1);
         int intMin = (int) (-1 * Math.pow(2, 31)), intMax = (int) (Math.pow(2, 31) - 1);
         long longMin = (long) (-1 * Math.pow(2, 63)), longMax = (long) (Math.pow(2, 63) - 1);
 
-        /*System.out.print("Give me the number!\n$ ");
-        theNumber = range.nextDouble();*/
-        DecimalFormat decFormat = new DecimalFormat("0.#");
 
-        for(int i = 0; i < 6; i++){
+        int Times = range.nextInt();
 
-            theNumber = range.nextDouble();
+        for(int i = 0; i < Times; i++){
+
+            var theNumber = range.nextDouble();
 
             if (theNumber < longMin || theNumber > longMax)
-                System.out.println(decFormat.format(theNumber) + " can't be fitted anywhere.");
+                System.out.println(theNumber + " can't be fitted anywhere.");
             else if (theNumber >= byteMin && theNumber <= byteMax)
                 System.out.println(decFormat.format(theNumber) + " can be fitted in:\n* byte\n* short\n* int\n* long");
             else if(theNumber >= shortMin && theNumber <= shortMax)
